680f3e17e2272aa07fcd3b410d5b666d8096810a
Emanuel Marcos <105237236+ebarbozadev@users.noreply.github.com>
2023-07-04 17:57:42 -0300

    Atualizando arquivos

---
+++ Dicas e Truques/VCL e FMX no mesmo Projeto/Unit1.pas
@@ -0,0 +1,27 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, FMXFormCadastro;
+
+type
+  TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    Cadastro1: TMenuItem;
+    Produtos1: TMenuItem;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
